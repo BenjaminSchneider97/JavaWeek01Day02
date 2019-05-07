f0ecda3cd6de1cc0fc7ae4c7be4afb770596f1ee
Christian Simic <christian.simicibwl@hotmail.com>
2019-05-07 14:47:00 +0200

    A4) completed

---
+++ src/IntermediateA5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class IntermediateA5 {
+
+    public static void main(String[] args) {
+
+        Scanner num = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = num.nextInt();
+
+        if (n % 2 == 0) {
+
+            System.out.println("Wir haben hier eine gerade Zahl");
+        } else {
+            System.out.println("Hier handelt es sich um eine ungerade Zahl");
+        }
+    }
+}
