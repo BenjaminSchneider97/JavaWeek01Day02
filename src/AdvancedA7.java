2c4e64c58fe61cb64f251e5ec6b140eb8feeca81
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-05-07 15:55:47 +0200

    add = to all if statements

--- src/AdvancedA7.java
+++ src/AdvancedA7.java
@@ -14,18 +14,18 @@ public class AdvancedA7 {
         System.out.print("Enter the third number: ");
         double c = num.nextDouble();
 
-        if (a < b && b < c){
-            System.out.println("The highest number is " + c + ", the second highest number is " + b + " and the smallest number is " + a);
-        } else if (a < c && c < b){
-            System.out.println("The highest number is " + b + ", the second highest number is " + c + " and the smallest number is " + a);
-        } else if (b < c && c < a){
-            System.out.println("The highest number is " + a + ", the second highest number is " + c + " and the smallest number is " + b);
-        } else if (b < a && a < c){
-            System.out.println("The highest number is " + c + ", the second highest number is " + a + " and the smallest number is " + b);
-        } else if (c < a && a < b){
-            System.out.println("The highest number is " + b + ", the second highest number is " + a + " and the smallest number is " + c);
-        } else if (c < b && b < a){
-            System.out.println("The highest number is " + a + ", the second highest number is " + b + " and the smallest number is " + c);
+        if (a <= b && b <= c){
+            System.out.println("A) The highest number is " + c + ", the second highest number is " + b + " and the smallest number is " + a);
+        } else if (a <= c && c <= b){
+            System.out.println("B) The highest number is " + b + ", the second highest number is " + c + " and the smallest number is " + a);
+        } else if (b <= c && c <= a){
+            System.out.println("C) The highest number is " + a + ", the second highest number is " + c + " and the smallest number is " + b);
+        } else if (b <= a && a <= c){
+            System.out.println("D) The highest number is " + c + ", the second highest number is " + a + " and the smallest number is " + b);
+        } else if (c <= a && a <= b){
+            System.out.println("E) The highest number is " + b + ", the second highest number is " + a + " and the smallest number is " + c);
+        } else if (c <= b && b <= a){
+            System.out.println("F) The highest number is " + a + ", the second highest number is " + b + " and the smallest number is " + c);
         }
     }
 }
