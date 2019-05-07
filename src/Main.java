ff46996441004c29c3de4911cc497eaa40f847d7
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-05-07 11:42:07 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = in.nextInt();
+
+        if (n < 0) {
+            System.out.println("You entered " + n + ", which is a negative Number!");
+        } else {
+            System.out.println("You entered " + n + ", which is a positive Number!");
+        }
+
+    }
+}
